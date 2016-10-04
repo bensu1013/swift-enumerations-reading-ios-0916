f4dbd79f4e155ab3aacd15164f8d3b7602594f66
Ben Su <bensu1013@hotmail.com>
2016-10-04 09:12:11 -0400

    I like Planets, Jim

--- Directions.playground/Contents.swift
+++ Directions.playground/Contents.swift
@@ -1,53 +1,25 @@
-func playerDidMove(_ direction: String) {
-    if direction == "up" ||
-       direction == "down" ||
-       direction == "left" ||
-       direction == "right" {
-        print("Player moved \(direction)")
-    } else {
-        print("Oops! That direction doesn't make sense: \(direction)")
-    }
+enum Planet : Int {
+    case Mercury = 1
+    case Venus
+    case Earth
+    case Mars
+    case Jupiter
+    case Saturn
+    case Uranus
+    case Neptune
+    
+    
 }
 
-playerDidMove("up")
-playerDidMove("left")
-playerDidMove("dwn")
-playerDidMove("Up")
 
-
-
-enum Direction {
-    case up
-    case down
-    case left
-    case right
-
-    var excited: String {
-        switch self {
-        case .up:
-            return "UP"
-        case .down:
-            return "DOWN"
-        case .left:
-            return "LEFT"
-        case .right:
-            return "RIGHT"
-        }
-    }
+func whereAreWe(planet : Planet) {
+    print("We are currently passing the planet \(planet). It is the \(planet.rawValue) planet in the Solar System")
 }
 
-let direction1 = Direction.up
-let direction2: Direction = .down
-var direction3: Direction = .right
+whereAreWe(planet: Planet.Mars)
 
-let direction = Direction.left
+let jupiter: Planet = .Jupiter
+let earth = Planet.Earth
 
-func playerDidMove(_ direction: Direction) {
-    print("Player moved \(direction.excited)")
-}
-
-playerDidMove(direction1)
-playerDidMove(direction2)
-playerDidMove(direction3)
-
-playerDidMove(.left)
+whereAreWe(planet: jupiter)
+whereAreWe(planet: earth)
